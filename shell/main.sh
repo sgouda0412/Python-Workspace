019b973bce95915960547a11ad1dcb2a49a916ea
santosh gouda <santosh.gouda@transformco.com>
2024-07-30 16:08:24 +0530

    commit

--- shell/main.sh
+++ shell/main.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Hello Bash Programming ......"
+function sendmail () {
+    echo "Hello Bash Programming...." # arguments are accessible through $1, $2,...
+}
+
+sendmail
+
+while [ 1 -lt 2 ]; do
+    let 
+done
+
