c8c403f4e86df2d390ac517bee565a5d451b0eb6
Santosh Gouda <sgouda0412@gmail.com>
2024-07-31 09:08:37 +0530

    commit

--- shell/main.sh
+++ shell/main.sh
@@ -1,3 +1,5 @@
+#[30-07-24] ===========================================================================================
+
 #!/usr/bin/env bash
 
 echo "Hello Bash Programming ......"
@@ -19,8 +21,43 @@ BASH_VERSION=$(bash --version)
 echo "$PWD"
 echo "$BASH_VERSION"
 
-myarray=(foo bar)
-for f in "${myarray[@]}"; do
-    cat "$f"
+# myarray=(foo bar)
+# for f in "${myarray[@]}"; do
+#     cat "$f"
+# done
+#========================================================================================================
+#[31-07-24]
+number=1
+
+while [[ $number -le 10 ]]; do
+    echo "$number"
+    ((number++))
 done
-git config --global core.autocrlf true
+echo "Now, number is $number"
+
+number=1
+until [[ $number -ge 10 ]]; do
+    echo "$number"
+    ((number++))
+done
+echo "Now, number is $number"
+
+devops_tools=("docker" "kubernetes" "ansible" "terraform" "jenkins")
+# shellcheck disable=SC2068
+for tool in ${devops_tools[@]}; do
+    # shellcheck disable=SC2086
+    echo $tool
+done
+
+echo "Numbers:"
+for number in 0 1 2 3 4 5 6 7 8 9; do
+    echo $number
+done
+echo "Names:"
+for name in Joe David Matt John Timothy; do
+    echo $name
+done
+echo "Files in current folder:"
+for file in $(pwd)/*; do
+    echo $file
+done
