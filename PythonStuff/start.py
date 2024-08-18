2fb4f5cd9cb253444e1f567ff24fd3194fee2471
sgouda0412 <sgouda0412@gmail.com>
2024-08-18 20:22:04 +0530

    Initial Commit

--- PythonStuff/start.py
+++ PythonStuff/start.py
@@ -1,3 +1,18 @@
+from collections import (
+    Counter,
+    UserDict,
+    UserList,
+    UserString,
+    defaultdict,
+    deque,
+    OrderedDict,
+    namedtuple,
+)
+
+x = Counter([1, 1, 2, 1, 2, 1, 3])
+print(x)
+
+
 def main():
     print("Hello world from the python world of jokers...")
 
